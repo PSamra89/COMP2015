b9c26dacd1d8a231d87166e7effe82f5313c80ad
PSamra89 <psamra14@my.bcit.ca>
2018-03-24 11:38:10 -0700

    first commit for comp 2019
    javascript

---
+++ Lab1/pavan-samra.js
@@ -0,0 +1,9 @@
+/*
+Name: Pavan Samra
+Student ID: A00891410
+ */
+
+var birdImage = document.getElementById("bird");
+var catImage = document.getElementById("cat");
+
+birdImage.width = catImage.width;
